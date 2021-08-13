ccd55ae966609fca19503b47dee71f147c2e947c
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2021-08-13 03:24:20 +0200

    Added MOD to main template, added json version.

--- Templates/main_template.cpp
+++ Templates/main_template.cpp
@@ -13,6 +13,7 @@ template <typename T, typename... Args> auto make(T init, size_t first, Args... 
 }
 
 const int INF = 2e9;
+const int MOD = 1e9 + 7;
 
 auto main() -> int {
     cin.tie(0)->sync_with_stdio(0);
