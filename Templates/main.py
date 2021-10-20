7a54e504d451abc9140aac66bf11b07c11f472a0
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2021-10-21 01:26:05 +0200

    Renamed files and added Python template.

---
+++ Templates/main.py
@@ -0,0 +1,8 @@
+from sys import stdin, stdout, setrecursionlimit; setrecursionlimit(200000)
+from math import gcd
+from heapq import heapify, heappush, heappop
+from functools import reduce
+from itertools import permutations
+
+INF = int(1e20)
+
