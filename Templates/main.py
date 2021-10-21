016424be2fe01cdc3a8ee58198a543a4f4089f8d
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2021-10-21 02:29:15 +0200

    Added product() to Python template.

--- Templates/main.py
+++ Templates/main.py
@@ -2,7 +2,7 @@ from sys import stdin, stdout, setrecursionlimit; setrecursionlimit(200000)
 from math import gcd
 from heapq import heapify, heappush, heappop
 from functools import reduce
-from itertools import permutations
+from itertools import product, permutations
 
 INF = int(1e20)
 
