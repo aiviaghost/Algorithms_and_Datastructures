71a193aec4efb48b89b4fad15826e2f2ab3d74e2
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2021-12-30 01:43:47 +0100

    Add Java template.

---
+++ Templates/main.java
@@ -0,0 +1,20 @@
+package fileName;
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class fileName {
+
+    final int INF = (int) 2e9;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        
+
+        out.flush();
+        out.close();
+    }
+}
