bd744fbc643ca4a977da94dbeff09b9257f6e871
aiviaghost <smurfplatta@gmail.com>
2022-10-27 00:48:24 +0200

    Change order in cpp template

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -13,15 +13,16 @@ template <typename T, typename... Args> auto make(T init, size_t first, Args... 
     return vector<decltype(inner)>(first, inner);
 }
 
-#define all(xs) xs.begin(), xs.end()
-#define in(xs, a) (xs.find(a) != xs.end())
-
-const int INF = 2e9;
-const int MOD = 1e9 + 7;
-
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 #define unordered_map gp_hash_table
+
+#define all(xs) xs.begin(), xs.end()
+#define in(xs, a) (xs.find(a) != xs.end())
+#define size(x) (ll)(x).size()
+
+const int INF = 2e9;
+const int MOD = 1e9 + 7;
 
 auto main() -> int {
     cin.tie(0)->sync_with_stdio(0);
