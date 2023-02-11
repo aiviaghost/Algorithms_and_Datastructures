93a5a6f251d5aae8a4f3fe0ee4eeaa1009c43ab1
aiviaghost <smurfplatta@gmail.com>
2023-02-11 19:42:24 +0100

    Fix order of macros

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+#define unordered_map gp_hash_table
+
+#define int ll
+#define all(xs) xs.begin(), xs.end()
+#define in(xs, a) (xs.find(a) != xs.end())
+#define size(x) (ll)(x).size()
+
 using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
@@ -12,15 +21,6 @@ template <typename T, typename... Args> auto make(T init, size_t first, Args... 
     auto inner = make<T>(init, sizes...);
     return vector<decltype(inner)>(first, inner);
 }
-
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-#define unordered_map gp_hash_table
-
-#define int ll
-#define all(xs) xs.begin(), xs.end()
-#define in(xs, a) (xs.find(a) != xs.end())
-#define size(x) (ll)(x).size()
 
 const int INF = 2e9;
 const int MOD = 1e9 + 7;
