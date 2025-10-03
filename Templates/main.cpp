ff5b908765bf75dc4a900c2d004ef8ffc797d9a6
aiviaghost <smurfplatta@gmail.com>
2025-10-03 21:38:43 +0200

    Update C++ template

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -4,20 +4,25 @@ using namespace std;
 
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
-#define unordered_map gp_hash_table
+// #define unordered_map gp_hash_table
 
 #define int ll
 #define all(xs) xs.begin(), xs.end()
 #define in(xs, a) (xs.find(a) != xs.end())
-#define size(x) (ll)(x).size()
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
 
 using ll = long long;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 using pdd = pair<double, double>;
+using vi = vector<int>;
 
-template <typename T> vector<T> make(T init, size_t size) { return vector<T>(size, init); }
-template <typename T, typename... Args> auto make(T init, size_t first, Args... sizes) {
+template <typename T>
+vector<T> make(T init, size_t size) { return vector<T>(size, init); }
+template <typename T, typename... Args>
+auto make(T init, size_t first, Args... sizes)
+{
     auto inner = make<T>(init, sizes...);
     return vector<decltype(inner)>(first, inner);
 }
@@ -25,8 +30,7 @@ template <typename T, typename... Args> auto make(T init, size_t first, Args... 
 const int INF = 2e9;
 const int MOD = 1e9 + 7;
 
-signed main() {
+signed main()
+{
     cin.tie(0)->sync_with_stdio(0);
-
-    
 }
